413723e55ed104c2ccb7e6360acb40c7a2831f3d
Zeeshan Yousuf <zeeshan.yousuf@orases.com>
2026-04-08 18:37:13 -0700

    Synth Grid - web-based music synthesizer

---
+++ server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const PORT = 3001;
+
+const MIME = {
+  '.html': 'text/html',
+  '.js':   'application/javascript',
+  '.css':  'text/css',
+  '.ico':  'image/x-icon',
+};
+
+http.createServer((req, res) => {
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html' : req.url);
+  const ext = path.extname(filePath);
+  const contentType = MIME[ext] || 'text/plain';
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': contentType });
+    res.end(data);
+  });
+}).listen(PORT, () => {
+  console.log(`\n  🎹 Synth Grid running at http://localhost:${PORT}\n`);
+});
